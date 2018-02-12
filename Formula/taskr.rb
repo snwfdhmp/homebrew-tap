ae85c83e1d1412e418130118e6c256d2fc824e6f
Martin Joly <snwfdhmp@gmail.com>
2018-02-12 13:16:18 +0100

    update taskr download mode

--- Formula/taskr.rb
+++ Formula/taskr.rb
@@ -1,7 +1,7 @@
 class Taskr < Formula
   desc "taskr keeps your development going forward by applying no-regression policy and unit-tested-task completion."
   homepage "https://github.com/snwfdhmp/taskr"
-  url "https://github.com/snwfdhmp/taskr/releases/download/v0.2.0/taskr_0.2.0_macOS_64-bit.tar.gz", :using => :git
+  url "https://github.com/snwfdhmp/taskr/releases/download/v0.2.0/taskr_0.2.0_macOS_64-bit.tar.gz", :using => :curl
   version "0.2.0"
   sha256 "1e453ac5d2732950a282e8e80d98fea6cdacee5e7f9770971f3746d0050af6fe"
   
